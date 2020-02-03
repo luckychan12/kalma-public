fbe0c0e3e2d861b6c7a8cdddc9b0930a4dc8d215
FergusBentley <fergus.bentley@gmail.com>
2020-02-03 20:52:30 +0000

    Write unit tests for Router class  - Check instantiation  - Check routing for valid uri and method  - Check routing for invalid uri  - Check routing for valid uri but invalid method

---
+++ webroot/api/test/Core/RouterTest.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+
+namespace Kalma\API\Test\Core;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+
+use FastRoute\Dispatcher;
+use Kalma\Api\Core\Router;
+use PHPUnit\Framework\TestCase;
+
+
+final class TestRouter extends TestCase
+{
+    public function testCanBeInstantiated() : Router
+    {
+        $router = new Router();
+        $this->assertInstanceOf(
+            Router::class,
+            $router
+        );
+        return $router;
+    }
+
+    /**
+     * @depends testCanBeInstantiated
+     * @param Router $router
+     */
+    public function testProvidesCorrectRouteForValidUri(Router $router) : void
+    {
+        $expected = array
+        (
+            'uri' => '/user/login',
+            'status' => Dispatcher::FOUND,
+            'resource' => 'User',
+            'action' => 'login',
+            'args' => array(),
+            'access_level' => 0,
+        );
+
+        $this->assertEquals($expected, $router->getRoute('POST', '/user/login'));
+    }
+
+    /**
+     * @depends testCanBeInstantiated
+     * @param Router $router
+     */
+    public function testFindsNoRouteForInvalidUri(Router $router) : void
+    {
+        $expected = array
+        (
+            'uri' => '/user/DUMMY_ROUTE',
+            'status' => Dispatcher::NOT_FOUND,
+        );
+
+        $this->assertEquals($expected, $router->getRoute('POST', '/user/DUMMY_ROUTE'));
+    }
+
+    /**
+     * @depends testCanBeInstantiated
+     * @param Router $router
+     */
+    public function testReturnsAllowedMethodsForInvalidMethod(Router $router) : void
+    {
+        $expected = array
+        (
+            'uri' => '/user/login',
+            'status' => Dispatcher::METHOD_NOT_ALLOWED,
+            'allowed_methods' => ['POST'],
+        );
+
+        $this->assertEquals($expected, $router->getRoute('GET', '/user/login'));
+    }
+}
