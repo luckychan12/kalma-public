00849965932699653b548cb7ca8d117f78c93805
FergusBentley <fergus.bentley@gmail.com>
2020-02-11 11:33:18 +0000

    Add API entry point  - index.php: passes HTTP Request to FrontController  - .htaccess: rewrites requests from api root to index.php

---
+++ webroot/api/public/index.php
@@ -0,0 +1,30 @@
+<?php
+
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7Server\ServerRequestCreator;
+use Kalma\Api\Core\FrontController;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+/* Set CORS Headers */
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Authorization");
+header("Access-Control-Allow-Methods: GET,HEAD,PUT,PATCH,POST,DELETE");
+
+$psr17Factory = new Psr17Factory();
+
+$requestCreator =
+    (
+    new ServerRequestCreator
+    (
+        $psr17Factory, // ServerRequestFactory
+        $psr17Factory, // UriFactory
+        $psr17Factory, // UploadedFileFactory
+        $psr17Factory  // StreamFactory
+    )
+    );
+
+$request = $requestCreator->fromGlobals();
+
+$frontController = new FrontController();
+$frontController->dispatchRequest($request);
