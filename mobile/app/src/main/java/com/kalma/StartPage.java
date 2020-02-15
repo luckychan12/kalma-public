1b2a33d790a52c1df02b1dfe0a5057aec51bf9fa
Ivan Chan <ivan.chan@students.plymouth.ac.uk>
2020-02-15 15:16:44 +0000

    Create start page Add Log-in button Add Sign-up button

---
+++ mobile/app/src/main/java/com/kalma/StartPage.java
@@ -0,0 +1,14 @@
+package com.kalma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_start_page);
+    }
+}
