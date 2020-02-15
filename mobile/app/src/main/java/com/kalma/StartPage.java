343a81a6e34815cec97a2b8ed479f2a71e00114d
Ivan Chan <ivan.chan@students.plymouth.ac.uk>
2020-02-15 16:40:09 +0000

    Add code to connect to login and signup activities

--- mobile/app/src/main/java/com/kalma/StartPage.java
+++ mobile/app/src/main/java/com/kalma/StartPage.java
@@ -2,13 +2,43 @@ package com.kalma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartPage extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.app_start_page);
+
+        Button loginButton = findViewById(R.id.btnLogin);
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                 openLoginPage();
+            }
+        });
+
+        Button signUpButton = findViewById(R.id.btnSignUp);
+        signUpButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openSignUpPage();
+            }
+        });
     }
+
+    private void openLoginPage(){
+        //TODO Create login page
+        //Intent intent = new Intent(this, LoginActivity.class)
+    }
+
+    private void openSignUpPage(){
+        //TODO Create login page
+        //Intent intent = new Intent(this, LoginActivity.class)
+    }
+
 }
