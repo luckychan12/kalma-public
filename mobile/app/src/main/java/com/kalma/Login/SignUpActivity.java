6bf6a74c1b6d99e854cef1eaa303d93c82274b96
Ivan Chan <ivan.chan@students.plymouth.ac.uk>
2020-02-16 22:09:53 +0000

    Create new activity for SignUp

---
+++ mobile/app/src/main/java/com/kalma/Login/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.kalma.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kalma.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
