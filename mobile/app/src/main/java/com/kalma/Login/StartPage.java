d04e1d3165a88986b1317249fcf71b37c7a6cc87
Ivan Chan <ivan.chan@students.plymouth.ac.uk>
2020-02-16 22:08:49 +0000

    Create new package for Login and SignUp Activities

--- mobile/app/src/main/java/com/kalma/Login/StartPage.java
+++ mobile/app/src/main/java/com/kalma/Login/StartPage.java
@@ -1,12 +1,14 @@
-package com.kalma;
+package com.kalma.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.kalma.Login.LoginActivity;
+import com.kalma.R;
 
 public class StartPage extends AppCompatActivity {
     @Override
