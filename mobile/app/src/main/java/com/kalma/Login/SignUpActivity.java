0c1dd31e5f0bd69d75387bc0a3b6e1b5989265f9
Ivan Chan <ivan.chan@students.plymouth.ac.uk>
2020-02-27 22:39:07 +0000

    Clean up code and add comments

--- mobile/app/src/main/java/com/kalma/Login/SignUpActivity.java
+++ mobile/app/src/main/java/com/kalma/Login/SignUpActivity.java
@@ -1,7 +1,5 @@
 package com.kalma.Login;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,9 +10,10 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.VolleyError;
 import com.kalma.API_Interaction.APICaller;
-import com.kalma.API_Interaction.AuthStrings;
 import com.kalma.API_Interaction.ServerCallback;
 import com.kalma.R;
 
@@ -35,8 +34,8 @@ import java.util.Locale;
 //TODO Implement error handling and data validation
 
 public class SignUpActivity extends AppCompatActivity {
-    EditText txtFirstName, txtLastName, txtPassword, txtEmail, txtDOB;
     final Calendar myCalendar = Calendar.getInstance();
+    EditText txtFirstName, txtLastName, txtPassword, txtEmail, txtDOB;
     Button signUp;
 
     @Override
@@ -53,6 +52,7 @@ public class SignUpActivity extends AppCompatActivity {
 
 
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            //Convert datepicker value into string and fill textbox
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear,
                                   int dayOfMonth) {
@@ -72,9 +72,11 @@ public class SignUpActivity extends AppCompatActivity {
                 String lastName = txtLastName.getText().toString();
                 String password = txtPassword.getText().toString();
                 String email = txtEmail.getText().toString();
+                //convert date string into DateTime object and generate epoch value
                 DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyy");
                 DateTime dateTimeGMT = new DateTime(formatter.parseDateTime(txtDOB.getText().toString()), DateTimeZone.UTC);
                 long epochSecs = (dateTimeGMT.getMillis() / 1000);
+                //Attempt to sign up
                 signUp(firstName, lastName, password, email, epochSecs);
 
             }
@@ -100,11 +102,13 @@ public class SignUpActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(JSONObject response) {
                         try {
+                            //tell user that the signup was successful
                             JSONObject responseBody = response;
                             String message = responseBody.getString("message");
                             Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
                             toast.show();
                             Log.d("Response", response.toString());
+                            //open login page
                             gotoLogin();
                         } catch (JSONException je) {
                             Log.e("JSONException", "onErrorResponse: ", je);
@@ -114,6 +118,7 @@ public class SignUpActivity extends AppCompatActivity {
                     @Override
                     public void onFail(VolleyError error) {
                         try {
+                            //tell user that signup failed and what the issue was
                             String jsonInput = new String(error.networkResponse.data, "utf-8");
                             JSONObject responseBody = new JSONObject(jsonInput);
                             String message = responseBody.getString("message");
