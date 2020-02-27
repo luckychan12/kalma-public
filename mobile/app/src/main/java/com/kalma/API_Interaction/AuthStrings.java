9f61d9ed65c75ded5e824add74e9d5ddce6a7dee
Ivan Chan <ivan.chan@students.plymouth.ac.uk>
2020-02-27 22:38:09 +0000

    Add comments

--- mobile/app/src/main/java/com/kalma/API_Interaction/AuthStrings.java
+++ mobile/app/src/main/java/com/kalma/API_Interaction/AuthStrings.java
@@ -3,17 +3,20 @@ package com.kalma.API_Interaction;
 import android.content.Context;
 import android.provider.Settings;
 
+//Auth strings singleton
 public class AuthStrings {
     private static AuthStrings authStrings;
     private static String authToken;
     private static String refreshToken;
     private static Context context;
 
+    //private Constructor
     private AuthStrings(Context ctx){
         context = ctx;
         authToken = getAuthToken();
     }
-    
+
+    //return instance of class if already created or call constructor
     public static synchronized  AuthStrings getInstance(Context context){
         if (authStrings == null){
             authStrings = new AuthStrings(context);
