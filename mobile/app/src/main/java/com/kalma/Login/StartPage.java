53cec83a1d55962a3e60f6b31cad2b316be25f00
Ivan Chan <ivan.chan@students.plymouth.ac.uk>
2020-03-26 06:54:49 +0000

    Implement reading refresh token from device

--- mobile/app/src/main/java/com/kalma/Login/StartPage.java
+++ mobile/app/src/main/java/com/kalma/Login/StartPage.java
@@ -3,24 +3,41 @@ package com.kalma.Login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.android.volley.VolleyError;
+import com.kalma.API_Interaction.APICaller;
+import com.kalma.API_Interaction.AuthStrings;
+import com.kalma.API_Interaction.ServerCallback;
 import com.kalma.Login.LoginActivity;
+import com.kalma.MainApp.HomeActivity;
 import com.kalma.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class StartPage extends AppCompatActivity {
+    public static final String MyPREFERENCES = "TOKENS";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.app_start_page);
-
+        attemptLoginRefresh();
         Button loginButton = findViewById(R.id.btnLogin);
         loginButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                 openLoginPage();
+
+                openLoginPage();
             }
         });
 
@@ -48,4 +65,37 @@ public class StartPage extends AppCompatActivity {
         //Do nothing
         //Prevents user from returning after logout
     }
+
+    private Map buildMap() {
+        Map<String, String> params = new HashMap<String, String>();
+        return params;
+    }
+
+    private void onSuccessfulLogin() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    private void attemptLoginRefresh() {
+        SharedPreferences settings = getApplicationContext().getSharedPreferences(MyPREFERENCES, 0);
+        String token = settings.getString("RefreshToken", "");
+        System.out.println("TOKEN =    \"" + token + "\"");
+        if (token == ""){
+            return;
+        }
+
+    }
+
+    private JSONObject buildRefreshObject(String token) {
+        //returns a json object based on input email and password
+        JSONObject object = new JSONObject();
+        try {
+            object.put("refresh_token", token);
+            object.put("client_fingerprint", AuthStrings.getInstance(this).getDeviceToken());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return object;
+    }
+
 }
