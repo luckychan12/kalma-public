000c74927950d89c2d3fe793875aa76661929adb
FergusBentley <fergus.bentley@gmail.com>
2020-04-06 20:25:49 +0100

    Add password hints

--- webroot/site/public/loginAndSignup.php
+++ webroot/site/public/loginAndSignup.php
@@ -32,7 +32,7 @@
                         <input class="form-control" type="email" placeholder="Email" name="logEmail" id="logEmail" required>
                     </div>
                     <div class="form-group">
-                        <label for="logPassword">Password:</label><br>
+                        <label for="logPassword">Password:</label>
                         <input class="form-control" type="password" placeholder="Password" name="logPassword" id="logPassword" required>
                     </div>
                     <input type="hidden" name="fingerprint" id="hiddenFingerprint">
@@ -43,7 +43,7 @@
             <div class="col-lg-5">
                 <hr class="d-lg-none">
                 <h2>Sign up</h2>
-                <form id="signupForm" class="form-signup"action="../controller/signUpController.php" method="post">
+                <form id="signupForm" class="form-signup" action="../controller/signUpController.php" method="post">
                     <div class="form-group">
                         <label for="email">Email Address:</label><br>
                         <input class="form-control" type="email" placeholder="Email" name="email" id="email" required>
@@ -57,7 +57,18 @@
                         <input class="form-control" type="text" placeholder="Last Name" name="lastName" id="lastName" required>
                     </div>
                     <div class="form-group">
-                        <label for="password">Password:</label><br>
+                        <label for="password">Password:</label>
+                        <a data-toggle="collapse" href="#passwordHints" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            <i class="fas fa-question"></i>
+                        </a>
+                        <div class="collapse" id="passwordHints">
+                                A strong password contains:
+                                <ul>
+                                    <li>At least 8 characters</li>
+                                    <li>Both uppercase and lowercase characters</li>
+                                    <li>Numeric and punctuation characters</li>
+                                </ul>
+                        </div>
                         <input class="form-control" type="password" placeholder="Password" name="password" id="password" required minlength="8">
                     </div>
                     <div class="form-group">
