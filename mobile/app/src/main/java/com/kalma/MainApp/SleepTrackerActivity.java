8a15e99e17e9a3635a32fd4690052df96dcd4f11
Ivan Chan <ivan.chan@students.plymouth.ac.uk>
2020-04-06 20:03:09 +0100

    Create Sleep tracker page

---
+++ mobile/app/src/main/java/com/kalma/MainApp/SleepTrackerActivity.java
@@ -0,0 +1,14 @@
+package com.kalma.MainApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SleepTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sleep_tracker);
+    }
+}
