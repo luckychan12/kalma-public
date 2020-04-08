47bb88879fd89137f645f9ecd76e6f1f9f41841c
Ivan Chan <ivan.chan@students.plymouth.ac.uk>
2020-04-08 20:08:40 +0100

    Implement code to make date picker work

--- mobile/app/src/main/java/com/kalma/MainApp/SleepTrackerActivity.java
+++ mobile/app/src/main/java/com/kalma/MainApp/SleepTrackerActivity.java
@@ -18,8 +18,16 @@ import android.widget.TimePicker;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
+import com.kalma.Data.AuthStrings;
 import com.kalma.Data.SleepDataEntry;
 import com.kalma.R;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -30,16 +38,18 @@ import java.util.Locale;
 public class SleepTrackerActivity extends AppCompatActivity {
 
     Context context = this;
-    EditText txtStartdate;
+    EditText txtStartDate, txtStopDate;
     Button buttonProfile, buttonSettings, buttonHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep_tracker);
+        setTitle("Sleep Tracker");
         final Calendar myCalendar = Calendar.getInstance();
-        TimePicker picker=(TimePicker)findViewById(R.id.timePicker1);
-        picker.setIs24HourView(true);
+
+        ((TimePicker)findViewById(R.id.startTimePicker)).setIs24HourView(true);
+        ((TimePicker)findViewById(R.id.stopTimePicker)).setIs24HourView(true);
         Spinner spinner = (Spinner) findViewById(R.id.option_spinner);
         List<String> categories = new ArrayList<String>();
         for (int i = 5; i >= 1; i--) {
@@ -49,7 +59,9 @@ public class SleepTrackerActivity extends AppCompatActivity {
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(dataAdapter);
 
-        txtStartdate = (EditText)findViewById(R.id.txtStartdate);
+        txtStartDate = (EditText)findViewById(R.id.txtStartdate);
+        txtStopDate = (EditText)findViewById(R.id.txtStopdate);
+        final EditText[] selecting = {null};
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             //Convert datepicker value into string and fill textbox
             @Override
@@ -59,17 +71,25 @@ public class SleepTrackerActivity extends AppCompatActivity {
                 myCalendar.set(Calendar.MONTH, monthOfYear);
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
                 SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.UK);
-                txtStartdate.setText(sdf.format(myCalendar.getTime()));
+                selecting[0].setText(sdf.format(myCalendar.getTime()));
             }
         };
         final DatePickerDialog datePicker = new DatePickerDialog(context);
-        txtStartdate.setOnClickListener(new View.OnClickListener() {
+        datePicker.setOnDateSetListener(date);
+        txtStartDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                selecting[0] = txtStartDate;
                 datePicker.show();
             }
         });
-
+        txtStopDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selecting[0] = txtStopDate;
+                datePicker.show();
+            }
+        });
         datePicker.setOnShowListener(new DialogInterface.OnShowListener() {
             @Override
             public void onShow(DialogInterface arg0) {
@@ -114,6 +134,7 @@ public class SleepTrackerActivity extends AppCompatActivity {
 
         }
     }
+
 }
 
 
