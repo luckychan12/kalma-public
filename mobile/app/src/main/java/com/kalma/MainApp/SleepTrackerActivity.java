68c8e37cbe564b81eb33791a037049e84d68b0ec
Ivan Chan <ivan.chan@students.plymouth.ac.uk>
2020-04-08 17:39:00 +0100

    Add code to make UI inputs work Implemented: - Made combo picker - Date picker - Time picker

--- mobile/app/src/main/java/com/kalma/MainApp/SleepTrackerActivity.java
+++ mobile/app/src/main/java/com/kalma/MainApp/SleepTrackerActivity.java
@@ -1,33 +1,82 @@
 package com.kalma.MainApp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.kalma.Data.SleepDataEntry;
 import com.kalma.R;
 
-import org.joda.time.DateTime;
-
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 public class SleepTrackerActivity extends AppCompatActivity {
 
     Context context = this;
+    EditText txtStartdate;
     Button buttonProfile, buttonSettings, buttonHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep_tracker);
+        final Calendar myCalendar = Calendar.getInstance();
+        TimePicker picker=(TimePicker)findViewById(R.id.timePicker1);
+        picker.setIs24HourView(true);
+        Spinner spinner = (Spinner) findViewById(R.id.option_spinner);
+        List<String> categories = new ArrayList<String>();
+        for (int i = 1; i <= 5; i++) {
+            categories.add(Integer.toString(i));
+        }
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
 
+        txtStartdate = (EditText)findViewById(R.id.txtStartdate);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            //Convert datepicker value into string and fill textbox
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.UK);
+                txtStartdate.setText(sdf.format(myCalendar.getTime()));
+            }
+        };
+        final DatePickerDialog datePicker = new DatePickerDialog(context);
+        txtStartdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePicker.show();
+            }
+        });
+
+        datePicker.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface arg0) {
+                datePicker.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(getResources().getColor(R.color.NoColour, null));
+                datePicker.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.YesColour, null));
+            }
+        });
         buttonProfile = findViewById(R.id.btnProfile);
         buttonProfile.setOnClickListener(new View.OnClickListener() {
             @Override
