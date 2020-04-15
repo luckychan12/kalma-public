144513a6378ba7e314121507b6da6c1b7df9e735
FergusBentley <fergus.bentley@gmail.com>
2020-04-15 17:16:46 +0100

    Prevent fingerprinter trying to write to #hiddenFingerprint field if it doesn't exist.

--- webroot/site/public/components/fingerprinter.php
+++ webroot/site/public/components/fingerprinter.php
@@ -3,19 +3,22 @@
     function getFingerprint(job){
         let client = new ClientJS();
         let fingerprint = client.getFingerprint();
+
+        if(!job) return fingerprint;
+
         if (job === "refresh"){
             window.location.href = "refreshCheck.php?clientFingerprint=" + fingerprint;
         }
         else if ( job === "logout"){
             window.location.href = "../controller/logoutController.php?clientFingerprint=" + fingerprint;
         }
-
-        return fingerprint;
     }
 
     $(function(){
         let client = new ClientJS();
-        document.getElementById("hiddenFingerprint").value = client.getFingerprint();
+        let $hiddenFingerprint = $("#hiddenFingerprint");
+        if ($hiddenFingerprint.length)
+            $hiddenFingerprint.val(client.getFingerprint());
     })
 </script>
 
