c1b1ed71412ff47e21d9c495bb8beafd336b436d
FergusBentley <fergus.bentley@gmail.com>
2020-04-16 12:17:56 +0100

    Revamp Dashboard
    - Show last 7 days of wellbeing data using line charts
    - Show averages over last 7 days

---
+++ webroot/site/public/assets/scripts/dashboard.js
@@ -0,0 +1,47 @@
+$(function() {
+
+    // Enable tooltips
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // Get appropriate text color from CSS
+    const style = getComputedStyle(document.body);
+    const textColor = style.getPropertyValue('--c-text-on-bg');
+    // Functions to format tick labels
+    const countToString = (t) => t.toString();
+    const minsToString = (t) => t > 60 ? `${Math.floor(t / 60)}h` + (t % 60 ? `${t % 60}m` : '') : `${t % 60}m`;
+    // Create the charts
+    $(".chart").each(function() {
+        const ctx = $(this)[0].getContext('2d');
+        const chart = new Chart(ctx, {
+            type: 'line',
+            data: $(this).data('chart-data'),
+            options: {
+                legend: {
+                    display: false,
+                },
+                scales: {
+                    yAxes: [
+                        {
+                            ticks: {
+                                stepSize: 60,
+                                min: 0,
+                                callback: $(this).hasClass('logged') ? countToString : minsToString,
+                                fontColor: textColor,
+                            },
+                        },
+                    ],
+                    xAxes: [
+                        {
+                            ticks: {
+                                fontColor: textColor,
+                            },
+                            gridLines: {
+                                display: false,
+                            },
+                        },
+                    ],
+                }
+            }
+        });
+    });
+});
